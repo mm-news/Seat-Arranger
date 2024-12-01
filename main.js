bb45f02aedc40220c33a2f6e55fb7a46ffdf3ac7
Undefined <mmtv.news.tw@gmail.com>
2024-12-01 20:02:08 +0800

    Refactor room size input handling and update table rendering in index.html and main.js

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-document.addEventListener("load", function () {
-    document.getElementById("room-row").addEventListener("change", update_room)
-    document.getElementById("room-col").addEventListener("change", update_room)
-})
+document.getElementById("set-room-size-btn").addEventListener("click", update_room)
+update_room()
 
 /**
  * Updates the room layout based on the event triggered.
  * @param {Event} event - The event object.
  */
 function update_room(event) {
-    if (confirm("This is gonna clear all your seats, are you sure?")) {
-        document.getElementById("arranging-area").innerHTML = ""
-        let r = document.getElementById("room-row").getAttribute("value")
-        let c = document.getElementById("room-col").getAttribute("value")
-        for (let i = 0; i < r; i++) {
-            //TODO: rewrite the table element
+    //TODO: Move items back to the students list
+    console.log("Updating room layout")
+    if (event) {
+        if (!confirm("This is gonna clear all your seats, are you sure?")) {
+            return
         }
     }
+    document.getElementById("arranging-area").innerHTML = "" // FIXME
+    alert()
+    console.log("Recreating table")
+    let r = document.getElementById("room-row").getAttribute("value")
+    let c = document.getElementById("room-col").getAttribute("value")
+    let new_table = ""
+    for (let i = 0; i < r; i++) {
+        new_table += '<tr>'
+        for (let j = 0; j < c; j++) {
+            new_table += '<td></td>'
+        }
+        new_table += "</tr>"
+    }
+    document.getElementById("arranging-area").innerHTML = new_table
 }
