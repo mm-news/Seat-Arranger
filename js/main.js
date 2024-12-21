23eb25d1b3d728d6385118d1708b72acd90ee057
Undefined <mmtv.news.tw@gmail.com>
2024-12-21 21:03:57 +0800

    Add the very first `Student` class to manage student data and display information

--- js/main.js
+++ js/main.js
@@ -48,3 +48,54 @@ function update_room(event) {
 
     document.getElementById("arranging-area").innerHTML = new_arranging_area.innerHTML
 }
+
+// Students
+
+class Student {
+    /**
+     * The class representing a student.
+     * @param {Number} id The unique ID for the student. TODO: Change the type to string.
+     * @param {Number} r The row number of the student's seat; 0-indexed; -1 for unassigned.
+     * @param {Number} c The column number of the student's seat; 0-indexed; -1 for unassigned.
+     * @param {Array<Number>} avoid The list of student IDs to avoid in the + area (4 seats).
+     * @param {Array<Number>} avoid_plus The list of student IDs to avoid in the # area (8 seats).
+     */
+    constructor(id, r, c, avoid, avoid_plus) {
+        this.id = id
+        this.r = r
+        this.c = c
+        this.avoid = avoid
+        this.avoid_plus = avoid_plus
+    }
+
+    get card() {
+        let card = document.createElement("div")
+        card.classList.add("card")
+
+        let card_body = document.createElement("div")
+        card_body.classList.add("card-body")
+
+        let card_title = document.createElement("h5")
+        card_title.classList.add("card-title")
+        card_title.textContent = "Student #" + this.id
+        card_body.appendChild(card_title)
+
+        let card_subtitle = document.createElement("h6")
+        card_subtitle.classList.add("card-subtitle", "mb-2", "text-body-secondary")
+        card_subtitle.textContent = "Seat: R" + (this.r > 0 ? this.r : "?") + "C" + (this.c > 0 ? this.c : "?")
+        card_body.appendChild(card_subtitle)
+
+        let card_text_A = document.createElement("p")
+        card_text_A.classList.add("card-text")
+        card_text_A.textContent = "Avoid: " + this.avoid.join(", ")
+        card_body.appendChild(card_text_A)
+
+        let card_text_B = document.createElement("p")
+        card_text_B.classList.add("card-text")
+        card_text_B.textContent = "Avoid Plus: " + this.avoid_plus.join(", ")
+        card_body.appendChild(card_text_B)
+
+        card.appendChild(card_body)
+        return card
+    }
+}
